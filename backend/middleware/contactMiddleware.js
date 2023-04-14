b7124e5d15259da6a28f78201f3cde8d5798f7d8
Duane Villapando <villapando.duane@gmail.com>
2023-04-14 23:25:36 +0800

    also in server

--- backend/middleware/contactMiddleware.js
+++ backend/middleware/contactMiddleware.js
@@ -1,22 +1,33 @@
-const multer = require('multer')
-const path = require('path')
-
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
-     destination: (req, file, cb) => {
-          cb(null, "./images")
-     },
-     filename: (req, file, cb) => {
-          console.log(file)
-          cb(null, Date.now() + path.extname(file.originalname))
-     }
+  destination: (req, file, cb) => {
+    cb(null, "./images");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
 
-})
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    const filetypes = /jpeg|jpg|png|gif|heic|webp/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-const upload = multer({storage: storage}).single("photo")
+    if (mimetype && extname) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type'));
+    }
+  }
+}).single('photo');
 
 const contactMiddleware = {
-     upload
-}
+  upload
+};
 
-module.exports = contactMiddleware
+module.exports = contactMiddleware;
