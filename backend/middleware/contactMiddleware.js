a39b589e9f5be85bd1b3ce66be985720b2490363
Duane Villapando <villapando.duane@gmail.com>
2023-04-15 10:56:01 +0800

    saw

--- backend/middleware/contactMiddleware.js
+++ backend/middleware/contactMiddleware.js
@@ -1,22 +1,30 @@
-const multer = require('multer')
-const path = require('path')
-
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
-     destination: (req, file, cb) => {
-          cb(null, "./images")
-     },
-     filename: (req, file, cb) => {
-          cb(null, Date.now() + path.extname(file.originalname))
-     }
-})
+  destination: (req, file, cb) => {
+    cb(null, "./images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
 
-const upload = multer({storage: storage}).single('photo')
+const imageFilter = (req, file, cb) => {
+  // Accept images only
+  if (!file.mimetype.startsWith("image/")) {
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
 
+const upload = multer({
+  storage: storage,
+  fileFilter: imageFilter
+}).single('photo');
 
 const contactMiddleware = {
-     upload
-}
+  upload
+};
 
-
-module.exports = contactMiddleware
+module.exports = contactMiddleware;
